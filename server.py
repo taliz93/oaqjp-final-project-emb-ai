17656a067a750a63ffefd5a627456a4e56de1d47
taliz93 <216754548+taliz93@users.noreply.github.com>
2025-08-08 12:53:23 -0400

    task 6, flasky

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
+# T6: Homepage
 @app.route('/')
 def do_home_page():
     return render_template('index.html')
 
+# T6: process emotions
+@app.route('/emotionDetector')
+def do_detect_emote():
+    req_text = request.args.get('textToAnalyze')
+    res_json = emotion_detector(req_text)
+    out_text = f"For the given statement, the system response is 'anger': {res_json['anger']}, 'disgust': {res_json['disgust']}, 'fear': {res_json['fear']}, 'joy': {res_json['joy']} and 'sadness': {res_json['sadness']}. The dominant emotion is {res_json['dominant_emotion']}."
+    return out_text
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 5000)
