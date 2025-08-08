198872ee824d4cccce1d980caf7c67c9c2fa0422
taliz93 <216754548+taliz93@users.noreply.github.com>
2025-08-08 11:56:51 -0400

    finished step 2

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,2 +1,10 @@
+import requests
+
 def emotion_detector(text_to_analyze):
-    return "notimplementedexception"
+    api_url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    api_hdr =  {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
+    api_inp = {'raw_document': {'text': text_to_analyze}}
+    
+    api_res = requests.post(api_url, json = api_inp, headers = api_hdr)
+
+    return api_res.text
