4d6da57686cbefcd7a846a1bcfae517681c51631
taliz93 <216754548+taliz93@users.noreply.github.com>
2025-08-08 13:24:25 -0400

    task 8, linted

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+'''
+server.py
+Flask Server encapsulation of EmotionDetection package.
+Using IBM Watson for AI Analysis
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -6,17 +12,29 @@ app = Flask(__name__)
 # T6: Homepage
 @app.route('/')
 def do_home_page():
+    '''
+    do_home_page()
+    Just renders the homepage
+    '''
     return render_template('index.html')
 
 # T6: process emotions
 @app.route('/emotionDetector')
 def do_detect_emote():
+    '''
+    do_detect_emote()
+    Takes text input and returns a formatted string.
+    '''
     req_text = request.args.get('textToAnalyze')
     res_json = emotion_detector(req_text)
     # T7: Error handle
     if res_json['dominant_emotion'] == 'None':
         return "Invalid text! Please try again!"
-    out_text = f"For the given statement, the system response is 'anger': {res_json['anger']}, 'disgust': {res_json['disgust']}, 'fear': {res_json['fear']}, 'joy': {res_json['joy']} and 'sadness': {res_json['sadness']}. The dominant emotion is {res_json['dominant_emotion']}."
+    out_text = f"For the given statement, the system response is "\
+               f"'anger': {res_json['anger']}, 'disgust': {res_json['disgust']}, "\
+               f"'fear': {res_json['fear']}, 'joy': {res_json['joy']} and "\
+               f"'sadness': {res_json['sadness']}. "\
+               f"The dominant emotion is {res_json['dominant_emotion']}."
     return out_text
 
 if __name__ == "__main__":
